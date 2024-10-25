b7584c4a67f7853aa12d6d30fda52a0d88424a9b
Akshay Balamurugan <akbalamurugan@plansource.com>
2024-10-26 03:30:37 +0530

    Complete skills section.

--- src/components/skill_card.jsx
+++ src/components/skill_card.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import GitIcon from '../assets/git_icon.svg'
 
-const SkillCard = () => {
+const SkillCard = ({ icon, text }) => {
   return (
     <div className='flex flex-col gap-8 items-center justify-center h-[186px] w-[186px] border-black border-2 rounded px-6 py-6 shadow-xl bg-white hover:invert'>
-        <img src={GitIcon} className='h-14 w-14' />
-        <p className='text-xl font-bold'>Git</p>
+        <img src={icon} className='h-14 w-14' />
+        <p className='text-xl font-bold'>{text}</p>
     </div>
   )
 }
--- src/sections/skills.jsx
+++ src/sections/skills.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import ThinAndThickTitle from '../components/thin_and_thick_title'
 import SkillCard from '../components/skill_card'
+import GitIcon from '../assets/git_icon.svg'
+import JavascriptIcon from '../assets/javascript_icon.svg'
+import RubyIcon from '../assets/ruby_icon.svg'
+import RailsIcon from '../assets/rails_icon.svg'
+import MysqlIcon from '../assets/mysql_icon.svg'
+import JavaIcon from '../assets/java_icon.svg'
+import HtmlIcon from '../assets/html_icon.svg'
+import CssIcon from '../assets/css_icon.svg'
+import ReactIcon from '../assets/react_icon.svg'
+import TailwindIcon from '../assets/tailwind_icon.svg'
+
+
 
 const Skills = () => {
   return (
@@ -10,16 +22,16 @@ const Skills = () => {
                 <ThinAndThickTitle thin="My" thick="Skills" />
             </div>
             <div className="grid grid-cols-2 lg:grid-cols-5 justify-between gap-10 py-5">
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
+                <SkillCard icon={RubyIcon} text="Ruby" />
+                <SkillCard icon={RailsIcon} text="Rails" />
+                <SkillCard icon={JavaIcon} text="Java" />
+                <SkillCard icon={MysqlIcon} text="MySQL" />
+                <SkillCard icon={GitIcon} text="Git" />
+                <SkillCard icon={ReactIcon} text="React" />
+                <SkillCard icon={TailwindIcon} text="Tailwind" />
+                <SkillCard icon={HtmlIcon} text="HTML" />
+                <SkillCard icon={CssIcon} text="CSS" />
+                <SkillCard icon={JavascriptIcon} text="Javascript" />
             </div> 
         </div>
     </div>
