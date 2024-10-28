91a5bc399c4a48d07f266d8a0f2ad31901d2c899
Akshay Balamurugan <akbalamurugan@plansource.com>
2024-10-29 04:05:23 +0530

    add minor ui enhancement.

--- src/components/download_button.jsx
+++ src/components/download_button.jsx
@@ -3,7 +3,7 @@ import DownloadIcon from '../assets/download.svg'
 
 function DownloadButton() {
   return (
-    <button className="px-5 py-4 flex items-center justify-center bg-black gap-2 rounded hover:bg-slate-700">
+    <button className="px-5 py-4 flex items-center justify-center bg-black gap-2 rounded hover:bg-zinc-800 hover:cursor-pointer">
         <p className="text-xl text-white leading-6 font-semibold">Resume</p>
         <img className="w-5 h-5"src={DownloadIcon} />
     </button>
